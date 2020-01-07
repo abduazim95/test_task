c1599d20a911bf400ff3b852c16bcbb9c82de632
abduazim95 <abduazim95@gmail.com>
2020-01-07 16:41:52 +0600

    Создание 3 страниц (Вход, Приветсвие, Сообщения)

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Messages from '@/views/Messages.vue';
+import Login from '@/views/Login.vue';
+import Home from '@/views/Home.vue';
+
 Vue.use(VueRouter)
 
 const routes = [
-    
-];
+    { path: '/messages', component: Messages },
+    { path: '/login', component: Login },
+    { path: '/', component: Home }
+]
 
 export default new VueRouter({
     routes
