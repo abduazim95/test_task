06f1cf5fee1a034c1e5bffc1823997e41e82b21b
abduazim95 <abduazim95@gmail.com>
2020-01-07 17:54:12 +0600

    Приветственная страница

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
-    <div>
-        Home
-    </div>
+	<div class="container">
+		<div class="jumbotron">
+			<h1 class="display-4">Приветствуем Вас, дорогой гость!</h1>
+			<p class="lead">
+                Наслажлайтесь, читайте! Наш ДОМ ждет ВАС!
+            </p>
+			<hr class="my-4" />
+			<p>Администрация делает всё, чтобы Вам было у нас комфортно и уютно.</p>
+			<a class="btn btn-primary btn-lg" href="#" role="button">Начать</a>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            
-        }
-    }
-}
+	data() {
+		return {};
+	}
+};
 </script>
